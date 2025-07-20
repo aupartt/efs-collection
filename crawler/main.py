1025a56389bd524113307d0e91fab5678732a34f
Aurélien Partiot <aurelien.partiot@gmail.com>
2025-07-20 13:12:54 +0200

    Update test script for better visibility

--- crawler/main.py
+++ crawler/main.py
@@ -7,13 +7,20 @@ if __name__ == "__main__":
     data: list[LocationEvents] = asyncio.run(
         get_location_events(
             [
-                # "https://dondesang.efs.sante.fr/trouver-une-collecte/3343/sang/19-07-2025",
-                "https://efs.link/FNh76",
+                "https://dondesang.efs.sante.fr/trouver-une-collecte/3343/sang/19-07-2025",  # Url with lots of events (useful for testing)
+                # "https://efs.link/FNh76",
                 # "https://efs.link/RE2rS",
             ],
             headless=True,
             keep_alive=False,
         )
     )
-    for event in data.items:
-        print(event["url"])
+    for location in data.items:
+        print("-------------------------")
+        print(location["url"])
+        for event in location["events"]:
+            print("-------------------------")
+            print("date:", event["date"])
+            print("slots:", event["slots"])
+            print("type:", event["type"])
+            print("schedules:", len(event["schedules"]))
