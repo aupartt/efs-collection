4ea22e020d01c6a19f974a9662010f030857ebba
Aurélien Partiot <aurelien.partiot@gmail.com>
2025-07-28 11:53:45 +0200

    Reflect changes for data storage

--- collecte-info/get_codes_postaux.py
+++ collecte-info/get_codes_postaux.py
@@ -2,24 +2,20 @@ import json
 from pathlib import Path
 from api_carto_client.models.sampling_location_entity import SamplingLocationEntity
 
-PREFIXE_LIEUX = "data/groupement_"
+LOCATIONS_FILE = Path("data/locations.jsonl")
 
 
 if __name__ == "__main__":
     #check_api()
 
-    pwd = Path.cwd()
-    fichiers_lieux = pwd.glob(f"{PREFIXE_LIEUX}*.json")
+    postal_code = set()
 
-    codes_postaux = set()
+    with LOCATIONS_FILE.open("r", encoding="utf-8") as file:
+        for line in file:
+            location = SamplingLocationEntity.from_dict(json.loads(line))
+            postal_code.add(location.post_code)
 
-    for fichier_lieux in fichiers_lieux:
-        with fichier_lieux.open("r", encoding="utf-8") as fp:
-            for line in fp:
-                lieu = SamplingLocationEntity.from_dict(json.loads(line))
-                codes_postaux.add(lieu.post_code)
-
-    print(codes_postaux)
+    print(postal_code)
 
 
 
