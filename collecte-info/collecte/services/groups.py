0a2b0cff60cf5906bbc1e0c644b6f7faf67b53f9
Aurélien Partiot <aurelien.partiot@gmail.com>
2025-08-09 12:07:06 +0200

    Remove useless utils functions

--- collecte-info/collecte/services/groups.py
+++ collecte-info/collecte/services/groups.py
@@ -1,8 +1,8 @@
+import asyncio
 import logging
 
 from sqlalchemy import exists, select
 
-from collecte.services.utils import update_all
 from collecte.core.database import get_db
 from collecte.models.group import GroupModel
 from collecte.schemas.group import GroupSchema
@@ -29,7 +29,25 @@ async def group_exists(gr_code: str) -> GroupSchema | None:
         return results.scalars()
 
 
-async def save_groups(groups: list[GroupSchema]) -> None:
-    """Retrieve groups from API and add them to database"""
+async def add_group(group: GroupSchema) -> GroupModel:
+    """Add a group to database"""
+    async with get_db() as session:
+        try:
+            stmt = select(GroupModel).where(GroupModel.gr_code == group.gr_code)
+            result = await session.execute(stmt)
+            existing_group = result.scalar_one_or_none()
+            if existing_group:
+                for key, value in group.model_dump().items():
+                    setattr(existing_group, key, value)
+            else:
+                session.add(GroupModel(**group.model_dump()))
+        
+            await session.commit()
+        except Exception as e:
+            logger.error(f"Error adding group {group.gr_code}: {e}")
+            await session.rollback()
 
-    await update_all(groups, GroupModel)
+async def save_groups(groups: list[GroupSchema]) -> None:
+    """ADD/UPDATE all groups to database"""
+    tasks = [add_group(group) for group in groups]
+    await asyncio.gather(*tasks)
--- collecte-info/collecte/services/utils.py
+++ collecte-info/collecte/services/utils.py
@@ -46,13 +46,6 @@ async def api_to_pydantic(
     return [pydantic_model(**api_model.to_dict()) for api_model in api_models]
 
 
-async def api_to_sqlalchemy(
-    api_models: list, sqlalchemy_model: SQLAlchemyBaseModel
-) -> list[SQLAlchemyBaseModel]:
-    """Convert an API model to a Pydantic model"""
-    return [sqlalchemy_model(**api_model.to_dict()) for api_model in api_models]
-
-
 async def pydantic_to_sqlalchemy(
     pydantic_models: list[BaseModel], sqlalchemy_model: SQLAlchemyBaseModel
 ) -> list[SQLAlchemyBaseModel]:
@@ -71,62 +64,3 @@ async def sqlalchemy_to_pydantic(
         pydantic_model(**sqlalchemy_model.__dict__)
         for sqlalchemy_model in sqlalchemy_models
     ]
-
-
-async def update_all(items: list, db_schema: SQLAlchemyBaseModel):
-    """Update or create all items in the database asynchronously.
-
-    Args:
-        items: A list of items to update or create in the database.
-        db_schema: The SQLAlchemy model class representing the table schema.
-    """
-    if not items:
-        return
-
-    # Convert items to SQLAlchemy models if necessary
-    if isinstance(items[0], BaseModel):
-        items = await pydantic_to_sqlalchemy(items, db_schema)
-    elif not isinstance(items[0], SQLAlchemyBaseModel):
-        items = await api_to_sqlalchemy(items, db_schema)
-
-    async def _update_item(item: SQLAlchemyBaseModel) -> None:
-        async with get_db() as session:
-            try:
-                # Get the primary key from the model class
-                primary_key = inspect(item.__class__).primary_key
-                if not primary_key:
-                    logger.error("No primary key found for the item.")
-                    return
-
-                # Assuming single primary key for simplicity
-                pk_name = primary_key[0].name
-                pk_value = getattr(item, pk_name, None)
-
-                db_item = None
-                if pk_value:
-                    db_item = await session.get(db_schema, pk_value)
-
-                # Add new item
-                if not db_item:
-                    session.add(item)
-                    await session.commit()
-                    logger.debug(f"Added a new object in table {item.__tablename__}")
-                    return
-
-                # Item exists, and hasn't changed
-                if db_item == item:
-                    return
-
-                # Item exists, update it
-                for key, value in item.__dict__.items():
-                    setattr(db_item, key, value)
-                await session.commit()
-                logger.debug(f"Updated object {pk_value} in table {item.__tablename__}")
-            except Exception as e:
-                await session.rollback()  # Rollback in case of error
-                logger.error(
-                    f"Error updating {pk_value} in table {item.__tablename__}: {e}"
-                )
-
-    tasks = [_update_item(item) for item in items]
-    await asyncio.gather(*tasks)
