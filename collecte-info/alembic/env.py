1b9c67c56744fb44b4b6a8af04d6a2a3608f5f90
Aurélien Partiot <aurelien.partiot@gmail.com>
2025-08-10 10:17:15 +0200

    Reset alembic

--- collecte-info/alembic/env.py
+++ collecte-info/alembic/env.py
@@ -8,7 +8,8 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 from alembic import context
 
 from collecte.core.settings import settings
-from collecte.schemas.base import Base
+import collecte.models
+from collecte.models.base import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
