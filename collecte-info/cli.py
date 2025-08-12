d1f17b793f102fd32225bc6d63a0d21bb7b58f24
Aurélien Partiot <aurelien.partiot@gmail.com>
2025-08-12 22:41:43 +0200

    Add cli usage with file

--- collecte-info/cli.py
+++ collecte-info/cli.py
@@ -1,12 +1,13 @@
 import argparse
 import asyncio
+import json
 
 from collecte.tasks.collections import update_collections
 from collecte.tasks.groups import update_groups
 from collecte.tasks.locations import update_locations
 from collecte.tasks.schedules import update_schedules
 
-parser = argparse.ArgumentParser("Collecte info CLI")
+parser = argparse.ArgumentParser(prog="collecte-info", description="Collecte info CLI")
 parser.add_argument(
     "--groups",
     "-g",
@@ -31,17 +32,41 @@ parser.add_argument(
     action="store_true",
     help="Run the task to retrieve and save new schedules",
 )
+parser.add_argument(
+    "--file",
+    "-f",
+    default=None,
+    help="The path and name of the JSON file",
+)
+parser.add_argument(
+    "--format",
+    "-F",
+    choices=['JSON', 'JSONL'],
+    default='JSONL',
+    help="Format of the JSON data [JSON, JSONL]",
+)
 
+
+def load_data(fila_path: str, file_type: str = "JSONL"):
+    with open(fila_path) as file:
+        if file_type == "JSON":
+            return json.load(file)
+        return [json.loads(line) for line in file.readlines()]
+        
 
 async def main(params: argparse.Namespace):
+    data = None
+
+    if params.file:
+        data = load_data(params.file, params.format)
     if params.groups:
-        await update_groups()
-    if params.locations:
-        await update_locations()
-    if params.collections:
-        await update_collections()
-    if params.schedules:
-        await update_schedules()
+        await update_groups(data)
+    elif params.locations:
+        await update_locations(data)
+    elif params.collections:
+        await update_collections(data)
+    elif params.schedules:
+        await update_schedules(data)
 
 
 if __name__ == "__main__":
--- collecte-info/tests/test_cli.py
+++ collecte-info/tests/test_cli.py
@@ -1,4 +1,5 @@
 from argparse import Namespace
+import json
 from unittest.mock import MagicMock
 
 import pytest
@@ -14,6 +15,8 @@ def _mock_args():
     args.locations = False
     args.collections = False
     args.schedules = False
+    args.file = None
+    args.format = None
     return args
 
 
@@ -83,3 +86,72 @@ async def test_cli_schedules(mocker: MockerFixture, _mock_args):
     mock_update_locations.assert_not_awaited()
     mock_update_collections.assert_not_awaited()
     mock_update_schedules.assert_awaited_once()
+
+
+@pytest.mark.asyncio
+async def test_cli_file(mocker: MockerFixture, _mock_args):
+    _mock_args.schedules = True
+    _mock_args.file = "/path/to/data.json"
+    mock_file_content = [{"foo": 1, "bar": 2}]
+    
+    mock_update_groups = mocker.patch("cli.update_groups")
+    mock_update_locations = mocker.patch("cli.update_locations")
+    mock_update_collections = mocker.patch("cli.update_collections")
+    mock_update_schedules = mocker.patch("cli.update_schedules")
+    mock_load_data = mocker.patch("cli.load_data", return_value=mock_file_content)
+
+    await cli(_mock_args)
+
+    mock_update_groups.assert_not_awaited()
+    mock_update_locations.assert_not_awaited()
+    mock_update_collections.assert_not_awaited()
+    mock_update_schedules.assert_awaited_once_with(mock_file_content)
+    mock_load_data.assert_called_once_with(_mock_args.file, None)
+
+
+@pytest.mark.asyncio
+async def test_cli_load_data_json(mocker: MockerFixture, _mock_args):
+    _mock_args.schedules = True
+    _mock_args.file = "/path/to/data.json"
+    _mock_args.format = "JSON"
+    mock_file_content = [{"foo": 1, "bar": 2}, {"foo": 1, "bar": 2}]
+    mock_file_read = '[{"foo": 1, "bar": 2},\n{"foo": 1, "bar": 2}]'
+    
+    mock_update_groups = mocker.patch("cli.update_groups")
+    mock_update_locations = mocker.patch("cli.update_locations")
+    mock_update_collections = mocker.patch("cli.update_collections")
+    mock_update_schedules = mocker.patch("cli.update_schedules")
+    mock_open = mocker.mock_open(read_data=mock_file_read)
+    mocker.patch("cli.open", mock_open)
+
+    await cli(_mock_args)
+
+    mock_update_groups.assert_not_awaited()
+    mock_update_locations.assert_not_awaited()
+    mock_update_collections.assert_not_awaited()
+    mock_update_schedules.assert_awaited_once_with(mock_file_content)
+    mock_open.assert_called_once_with(_mock_args.file)
+
+
+@pytest.mark.asyncio
+async def test_cli_load_data_jsonl(mocker: MockerFixture, _mock_args):
+    _mock_args.schedules = True
+    _mock_args.file = "/path/to/data.json"
+    _mock_args.format = "JSONL"
+    mock_file_content = [{"foo": 1, "bar": 2}, {"foo": 1, "bar": 2}]
+    mock_file_read = '{"foo": 1, "bar": 2}\n{"foo": 1, "bar": 2}'
+    
+    mock_update_groups = mocker.patch("cli.update_groups")
+    mock_update_locations = mocker.patch("cli.update_locations")
+    mock_update_collections = mocker.patch("cli.update_collections")
+    mock_update_schedules = mocker.patch("cli.update_schedules")
+    mock_open = mocker.mock_open(read_data=mock_file_read)
+    mocker.patch("cli.open", mock_open)
+
+    await cli(_mock_args)
+
+    mock_update_groups.assert_not_awaited()
+    mock_update_locations.assert_not_awaited()
+    mock_update_collections.assert_not_awaited()
+    mock_update_schedules.assert_awaited_once_with(mock_file_content)
+    mock_open.assert_called_once_with(_mock_args.file)
