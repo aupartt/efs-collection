61948c91979ef9466aa9d245b00072bb1f9fba31
Aurélien Partiot <aurelien.partiot@gmail.com>
2025-08-12 14:36:11 +0200

    Add CRAWLER_BATCH

--- collecte-info/collecte/core/settings.py
+++ collecte-info/collecte/core/settings.py
@@ -3,16 +3,13 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    # RabbitMQ
-    RABBITMQ_URL: AmqpDsn = "amqp://guest:guest@localhost:5672/"
-    RABBITMQ_DATA_QUEUE: str = "processed_data"
-
     # Database
     POSTGRES_URL: PostgresDsn = (
         "postgresql+asyncpg://postgres:postgres@localhost:5432/collecte"
     )
 
     REGION_NAME: str = "Bretagne"
+    CRAWLER_BATCH: int = 20
 
     model_config = SettingsConfigDict(env_file=".env", case_sensitive=False)
 
