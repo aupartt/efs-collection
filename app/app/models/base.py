1415bd862c140da165caf9c1ac629bb9f852d9f8
Aurélien Partiot <aurelien.partiot@gmail.com>
2025-09-05 23:59:25 +0200

    Remove useless data models

--- app/app/models/base.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import JSON
-from sqlalchemy.ext.asyncio import AsyncAttrs
-from sqlalchemy.orm import DeclarativeBase
-
-
-class Base(DeclarativeBase, AsyncAttrs):
-    type_annotation_map = {dict: JSON}
