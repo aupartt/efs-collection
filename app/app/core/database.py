897dc5a30fce097456bbdc29ec46d619cc0a2a3a
Aurélien Partiot <aurelien.partiot@gmail.com>
2025-09-06 00:00:58 +0200

    Add tables class and decoration Session function

--- app/app/core/database.py
+++ app/app/core/database.py
@@ -1,3 +1,4 @@
+from sqlalchemy import MetaData, Table
 from sqlalchemy.engine import create_engine
 from sqlalchemy.orm import sessionmaker
 
@@ -6,11 +7,27 @@ from app.core.settings import settings
 engine = create_engine(settings.POSTGRES_URL, pool_size=10, max_overflow=20)
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 
+metadata = MetaData()
 
-def get_db():
-    """Dependency to get database session"""
-    with SessionLocal() as session:
-        try:
-            yield session
-        finally:
-            session.close()
+
+# Postgres tables
+class tables:
+    groups = Table("groups", metadata, autoload_with=engine)
+    locations = Table("locations", metadata, autoload_with=engine)
+    collection_groups = Table("collection_groups", metadata, autoload_with=engine)
+    collection_group_snapshots = Table("collection_group_snapshots", metadata, autoload_with=engine)
+    collection_events = Table("collection_events", metadata, autoload_with=engine)
+    schedules = Table("schedules", metadata, autoload_with=engine)
+
+
+def with_session(fn):
+    """Function decorator to get database session"""
+
+    def wrapper(*args, **kwargs):
+        with SessionLocal() as session:
+            try:
+                return fn(session, *args, **kwargs)
+            finally:
+                session.close()
+
+    return wrapper
